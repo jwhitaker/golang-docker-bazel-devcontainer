cbb736a5eb48df9090fd21858ff3fa6ec119e56f
Jason Whitaker <jason@thewhitakers.ca>
2022-09-18 07:24:16 -0400

    starting to test service

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 	"github.com/thewhitakers/echoer/pkg/echoer"
 )
 
 func main() {
 	svc := echoer.NewService()
+	e := echo.New()
 
-	log.Println(svc.GetMessage("Hello World"))
+	e.GET("/", func(c echo.Context) error {
+		msg := svc.GetDefaultMessage()
+		return c.String(http.StatusOK, msg)
+	})
+
+	e.POST("/", func(c echo.Context) error {
+		requestMsg := c.FormValue("msg")
+
+		msg := svc.GetMessage(string(requestMsg))
+		return c.String(http.StatusOK, msg)
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
 }
--- pkg/echoer/service.go
+++ pkg/echoer/service.go
@@ -4,6 +4,10 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	defaultMessage = "Hello World"
+)
+
 // Service is a struct to test how bazel deals with packages.
 type Service struct {
 }
@@ -11,6 +15,13 @@ type Service struct {
 // NewService will create a new Service instance
 func NewService() Service {
 	return Service{}
+}
+
+// GetDefaultMessage returns the default message
+func (srv Service) GetDefaultMessage() string {
+	log.Debugln("Getting default message")
+
+	return defaultMessage
 }
 
 // GetMessage will return the message given
